a8c2f55883351569f31a47ae18c2f80e5845a442
Tim Schilling <schillingt@better-simple.com>
2026-06-16 21:26:43 -0500

    Switch to the AWS managed state for Terraform.

--- terraform/repositories/backend.tf
+++ terraform/repositories/backend.tf
@@ -1,8 +1,9 @@
-# Backend Configuration
-# https://www.terraform.io/language/settings/backends/configuration
-
 terraform {
-  backend "local" {
-    path = "tfstate.json"
+  backend "s3" {
+    bucket         = "django-commons-terraform-state"
+    key            = "test/repositories/tfstate.json"
+    region         = "us-east-1"
+    dynamodb_table = "django-commons-terraform-state-lock"
+    encrypt        = true
   }
 }
